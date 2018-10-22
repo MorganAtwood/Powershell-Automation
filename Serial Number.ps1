2dd1e708d19f40e8704975d54fa5a59c4fda50c8
Morgan Atwood <17478450+MorganAtwood@users.noreply.github.com>
2018-10-22 11:26:15 -0500

    Rename Serial Number.ps1 to Get-SerialNumber.ps1

--- Serial Number.ps1
+++
@@ -1,9 +0,0 @@
-﻿
-## Morgan Atwood 5/5/2016
-# This program is designed to show you computers serial number
-#
-
-
-
-$computers = Get-Content C:\Users\atwoodm\Desktop\computerlist.txt
-$computers | foreach { Get-WmiObject -ComputerName $computers -Class Win32_BIOS | Select -Property PSComputerName,SerialNumber} | Out-File C:\Users\atwoodm\Desktop\computerlist_complete.txt
